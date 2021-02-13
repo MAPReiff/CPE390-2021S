03bb8bd9c4f86b167bb10b79a9b0ff866bb04bb1
Mitchell Reiff <mapr323@gmail.com>
2021-02-13 15:14:27 -0500

    Updated HW Formatting
    Updated formatting on HW1 & HW2

--- HW 1/hw.cc
+++ HW 1/hw.cc
@@ -1,46 +1,48 @@
 /*
-	I pledge my honor that I have abided by the Stevens Honor System
-	Author: Mitchell Reiff
+        I pledge my honor that I have abided by the Stevens Honor System
+        Author: Mitchell Reiff
 */
 #include <iostream>
 using namespace std;
 
 int sum(int a, int b) {
-  int answer = 0;
-  for (int i = a; i <= b; i++) {
-    answer += i;
-  }
-  return answer;
+	int answer = 0;
+	for (int i = a; i <= b; i++) {
+		answer += i;
+	}
+	return answer;
 }
 
 long int sumOfSquares(int c) {  // did long just in case
-  int answer = 0;
-  for (int i = 0; i <= c; i++) {
-    answer += i * i;
-  }
-  return answer;
+	int answer = 0;
+	for (int i = 0; i <= c; i++) {
+		answer += i * i;
+	}
+	return answer;
 }
 
 unsigned fact(int num) {
-  uint64_t total = 1;  // needs to be uint64
-  for (int i = 1; i <= num; i++) {
-    total *= i;  // total = total * uint64_t(i)
-  }
-  return total;
+	uint64_t total = 1;  // needs to be uint64
+	for (int i = 1; i <= num; i++) {
+		total *= i;  // total = total * uint64_t(i)
+	}
+	return total;
 }
 
 int main() {
-	cout << sum(3, 6) << '\n'; // compute the sum 3 + 4 + 5 + 6
-	cout << sum(5, 100) << '\n'; // compute the sum 5+6+7+...+100
-  cout << fact(5) << '\n'; //120
+	cout << sum(3, 6) << '\n';    // compute the sum 3 + 4 + 5 + 6
+	cout << sum(5, 100) << '\n';  // compute the sum 5+6+7+...+100
+	cout << fact(5) << '\n';      // 120
 	for (int i = 1; i <= 25; i++)
 		cout << i << ": " << fact(i) << '\n';
-  //cout << fact(50) << '\n';
-  cout << sumOfSquares(3) << '\n'; // compute 1*1 + 2*2 + 3*3 = 14
-  cout << sumOfSquares(11) << '\n'; // compute 1*1 + 2*2 + 3*3 + ... + 10*10+11*11
+	// cout << fact(50) << '\n';
+	cout << sumOfSquares(3) << '\n';  // compute 1*1 + 2*2 + 3*3 = 14
+	cout << sumOfSquares(11)
+	     << '\n';  // compute 1*1 + 2*2 + 3*3 + ... + 10*10+11*11
 }
 
-// It appears the hw file on github was modified since I had orginally pulled it. Here is the orginal main function.
+// It appears the hw file on github was modified since I had orginally pulled
+// it. Here is the orginal main function.
 /*
 int main() {  // added line breaks to the first 2 lines to make it nicer
   cout << sum(3, 6) << '\n';    // compute the sum 3 + 4 + 5 + 6
