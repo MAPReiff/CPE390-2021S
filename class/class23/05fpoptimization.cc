d86a9d7ff143b49731258fa15b81fb98ea922666
Mitchell <mapr323@gmail.com>
2021-04-22 13:40:23 -0400

    Class 23 4/22/21

---
+++ class/class23/05fpoptimization.cc
@@ -0,0 +1,40 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+class Vec3d {
+ public:
+  double x, y, z;
+  Vec3d(double x_, double y_, double z_) : x(x_), y(y_), z(z_) {}
+
+  friend Vec3d operator+(const Vec3d& a, const Vec3d& b) {
+    return Vec3d(a.x + b.x, a.y + b.y, a.z + b.z);
+    // as
+  }
+
+  double mag() const {
+    return sqrt(x * x + y * y + z * z);
+    //
+  }
+};
+
+int main() {
+  int secondsPerYear = 365 * 24 * 60 * 60;  // 315600000
+  // const expressions are pre-evaluated
+  double x = M_PI * 2;
+
+  int a[2 * 5];  // firguires out how big a is at compile time
+
+  double b;
+  cin >> b;
+
+  // a+b+c!=a+(b+c)
+
+  double c = b * 2.0 / 3;  // should generate vmul.f64 d0, d1, d2  vdiv.f64...
+  // double c = b * (2.0/3.0);
+
+  Vec3d pos(2.5, 10.2, -3.1);
+  Vec3d delta(1, -1, 0);
+  pos = pos + delta;
+  cout << pos.mag() * pos.mag();
+}
